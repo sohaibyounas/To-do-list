c503bdfbc6cb62725bb440b715c5f97b59090aae
sohaibyounas <sohaibyounas24@gmail.com>
2025-03-24 11:25:37 +0500

    update

---
+++ script.js
@@ -0,0 +1,44 @@
+// Add task
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value === "") {
+        alert("Please enter a task!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    storeTasks();
+}
+
+// click to add task
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        storeTasks();
+    } 
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        storeTasks()
+    }
+
+}, false);
+
+
+// Store Tasks
+function storeTasks() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// Get Tasks
+function getTasks() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+getTasks()
